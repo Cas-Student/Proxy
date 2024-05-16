b81c8da008befc5ba84105b953bdb2f57cf2a8c1
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-16 08:59:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
-    'username' : 'password',
+    'Iprox' : '1Q3W5E7R9T2Y4U6I8O0P.4.4.11',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
