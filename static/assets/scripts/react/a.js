8e2aecdb346077b5425bad5e8f7a3d24d73b5b37
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-16 19:59:43 -0400

    Delete static/assets/scripts/react directory

--- static/assets/scripts/react/a.js
+++
@@ -1,7 +0,0 @@
-function LoadTitle() {
-    return "Hacker Hub";
-}
-
-const container = document.getElementsByClassName("title");
-const root = ReactDom.createRoot(container);
-root.render(<LoadTitle />);
