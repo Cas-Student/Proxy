bc60af61ca35d53ede360ce7e2da334ed4d904f0
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-22 20:30:32 -0400

    Update accounts.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -1,10 +1,10 @@
 const Accounts = {
     'Wesley' : 'Password',
-    'Cas' : 'Password',
+    'Cas' : 'Password'
 }
 
 const icon = document.getElementsByClassName("account-icon");
-icon.src = "";
+icon.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F345418%2Faccount-circle&psig=AOvVaw3CpHpkIN69XRSIDCUB6B2j&ust=1716510614215000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDAzr_CooYDFQAAAAAdAAAAABAE";
 icon.alt = "accounts";
 
 export default Accounts;
