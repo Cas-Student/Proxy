cc0a97a5386056a2793c332cfe42d2754e8def72
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-23 22:12:22 -0400

    Update accounts.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -1,10 +1,3 @@
-const Accounts = {
-    'Wesley' : 'Password',
-    'Cas' : 'Password'
-}
-
 const icon = document.getElementsByClassName("account-icon");
 icon.src = "https://static.vecteezy.com/system/resources/previews/007/407/995/original/account-symbol-leader-and-workers-team-logo-vector.jpg";
 icon.alt = "Profile";
-
-export default Accounts;
