33010b5bc3ac0838e5956f91a32f176ed7cfeca2
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-26 13:56:47 -0400

    Update accounts.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -2,6 +2,5 @@ const icon = document.getElementsById("account-icon");
 icon.src = "https://static.vecteezy.com/system/resources/previews/007/407/995/original/account-symbol-leader-and-workers-team-logo-vector.jpg";
 icon.alt = "Profile";
 
-import IP from "../../../../index.js" | "./index.js";
-alert("You have been hacked...");
-alert('Your IP is: " + IP);
+//Hides main site
+document.getElementById("site").style.display = "none";
