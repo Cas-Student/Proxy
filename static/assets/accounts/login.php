b3d55bb36ac1ac610b721c379f2fe008756567be
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-26 18:46:50 -0400

    Update login.php
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/login.php
+++ static/assets/accounts/login.php
@@ -1,1 +1,18 @@
-echo "Testing";
+<?php
+$mysqli = mysqli_connect("ep-sweet-sound-a40jvt3t.us-east-1.pg.koyeb.app","koyeb-adm","R24IfvslAyqU"."koyebdb")
+
+if (mysqli_connect_errno() {
+  exit();
+} else {
+  $sql = "Select First FROM users";
+  $res = mysqli_query($mysqli, $sql);
+  if ($res === TRUE) {
+    $Pin = $newArray['Pin'];
+    $firstName = $newArray['First'];
+    $lastName = $newArray['Last'];
+    echo "<script>alert('Hello' + $firstName ' ' + $lastName + ', your pin is: ' + $Pin);</script>"
+  }
+  mysqli_close($mysqli);
+}
+
+?>
