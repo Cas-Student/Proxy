8f4132ebc8ccde7eac2c1b5b16ba13abb4f2d648
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-26 20:24:16 -0400

    Update accounts.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -4,3 +4,14 @@ icon.alt = "Profile";
 
 //Hides main site
 document.getElementById("site").style.display = "none";
+
+//Adds the CSS
+const link1 document.createElement("link");
+link1.rel="stylesheet";
+link1.href="/assets/styles/main.css";
+const link2 document.createElement("link");
+link2.rel="stylesheet";
+link2.href="/assets/styles/themes/default.css";
+
+document.head.appendChild(link1);
+document.head.appendChild(link2);
