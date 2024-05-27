1eb585402173cacd6762c1ea900a4c1091fff88a
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-27 16:47:33 -0400

    Update accounts.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -2,8 +2,9 @@ const icon = document.getElementById("account-icon");
 icon.src = "https://static.vecteezy.com/system/resources/previews/007/407/995/original/account-symbol-leader-and-workers-team-logo-vector.jpg";
 icon.alt = "Profile";
 
-//Hides main site
-document.getElementById("site").style.display = "none";
+//Hides main site of login screen.
+//site or login
+document.getElementById("login").style.display = "none";
 
 //Adds the CSS
 const link1 = document.createElement("link");
