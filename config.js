9c4924b0e426780797dcb01d5a2813825147cb4e
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-28 20:59:39 -0400

    Update config.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
-    'username' : 'password',
-  },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
 }
