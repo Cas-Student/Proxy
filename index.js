9a36163ec7b5b2a9aedcda34899fd1b604cc5f45
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-28 21:12:29 -0400

    Update index.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- index.js
+++ index.js
@@ -19,14 +19,7 @@ console.log("Running on port: " + PORT);
 //IP Finding
 app.get('/', (req, res) => {
   const ip = req.get('X-Forwarded-For');
-  console.log("ROOT IP: " + ip + "\n\n");
-  fetch('https://api.ipdata.co?api-key=18c53b9cfa59d61be7e075ec26ecfb7d1d4aafa653defa0d96a56950')
-  .then(res => res.json())
-  .then(data => {
-    console.log("Ip: " + data.ip);
-    console.log("City: " + data.city);
-    console.log("State: " + data.region);
-  });
+  console.log("\nROOT IP: " + ip + "\n");
   res.end();
 });
 
