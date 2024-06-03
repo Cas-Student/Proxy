d1ee476c3cc63683c828ae703841e68b501242df
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-06-03 22:24:37 +0000

    n

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -9,10 +9,16 @@ document.getElementById("login").style.display = "none";
 //Adds the CSS
 const link1 = document.createElement("link");
 link1.rel="stylesheet";
+link1.type="text/css";
 link1.href="/assets/styles/main.css";
 const link2 = document.createElement("link");
 link2.rel="stylesheet";
+link2.type="text/css";
 link2.href="/assets/styles/themes/default.css";
+const link3 = document.createElement("link");
+link3.rel="stylesheet";
+link3.type="text/css";
+link3.href="assets/accounts/login.css";
 
 document.head.appendChild(link1);
 document.head.appendChild(link2);
