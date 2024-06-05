f0fa86f1e6b16da28d2d76e14f1713f73174dfac
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-06-05 08:58:44 -0400

    Update users.js
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- static/assets/accounts/users.js
+++ static/assets/accounts/users.js
@@ -1,14 +1,1 @@
-const Accounts = {
-    'Alexandra' : 'Stanley',
-    'Cas' : 'Macino',
-    'Colton' : 'Miller',
-    'Evans' : 'Stokley',
-    'Parker' : 'Warren',
-    'Paul' : 'Allen',
-    'Rocco' : 'Preteroti',
-    'Jameson' : 'Mims',
-    'Vin' : 'Galczynski',
-    'Wesley' : 'Miller'
-}
 
-export default Accounts;
