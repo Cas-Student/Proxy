18fb521a0888c0d95aa0cfc7e92007687ca56ae9
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-06-07 23:57:09 +0000

    Turned on login

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -2,7 +2,7 @@ const icon = document.getElementById("account-icon");
 icon.src = "https://static.vecteezy.com/system/resources/previews/007/407/995/original/account-symbol-leader-and-workers-team-logo-vector.jpg";
 icon.alt = "Profile";
 
-const login = false;
+const login = true;
 if (login) {
     document.getElementById("site").style.display = "none";
     document.getElementById("login").style.display = "block";
