700eaa3c0f0fa9a59cfd5dc95f7743e40ee1e214
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-06-17 14:49:05 +0000

    debuggin inputs

--- static/assets/accounts/verify.php
+++ static/assets/accounts/verify.php
@@ -1,3 +1,7 @@
 <?php
+    $fName = $_REQUEST['fName'];
+    $lName = $_REQUEST['lName'];
+    $pin = $_REQUEST['pin'];
     echo "Helo World";
+    echo "input was fName=".fName."&lName=".lName."&pin=".pin;
 ?>
