dcd74d76110d2028753e58ba8571170e859a0274
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-06-17 15:06:38 +0000

    DB testing

--- static/assets/accounts/verify.php
+++ static/assets/accounts/verify.php
@@ -1,4 +1,30 @@
 <?php
+    $host = 'ep-sweet-sound-a40jvt3t.us-east-1.pg.koyeb.app';
+    $user='koyeb-adm';
+    $password='R24IfvslAyqU';
+    $database='users';
+    $connect = mysqli_connect($host);
+    //Checks to see if the connection was made
+    if (!$connect) {
+        echo "Could not connect to network";
+        exit(0);
+    }
+    //SQL code
+    $sql = 'SELECT * FROM *;';
+    //Queries SQL
+    $result = mysqli_query($connect,$sql);
+    //Checks to see if the query was sucessful
+    if (!$result) {
+        echo "Could not get information";
+        exit(0);
+    }
+    //Formats data
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    //Displays data
+    foreach ($rows as $row) {
+        echo $row['']":|<br>";
+    }
+    //PHP
     $fName = $_REQUEST['fName'];
     $lName = $_REQUEST['lName'];
     $pin = $_REQUEST['pin'];
