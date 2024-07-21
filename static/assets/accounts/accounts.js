b84b2ce3524c3c32e7a99987f5421d4aa2f28f39
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-21 23:31:00 +0000

    commit

--- static/assets/accounts/accounts.js
+++ static/assets/accounts/accounts.js
@@ -22,26 +22,6 @@ if (login && !localStorage.getItem('login')) {
     location.href = location.href;
 }
 
-//Adds the CSS
-alert("Adding CSS");
-
-const link1 = document.createElement("link");
-link1.rel="stylesheet";
-link1.type="text/css";
-link1.href="/assets/styles/main.css";
-const link2 = document.createElement("link");
-link2.rel="stylesheet";
-link2.type="text/css";
-link2.href="/assets/styles/themes/default.css";
-const link3 = document.createElement("link");
-link3.rel="stylesheet";
-link3.type="text/css";
-link3.href="assets/accounts/login.css";
-
-document.head.appendChild(link1);
-document.head.appendChild(link2);
-document.head.appendChild(link3);
-
 document.getElementById("mainLoginForm").addEventListener("submit", function() {
     const fName = document.forms["mainLoginForm"]["fName"].value;
     const lName = document.forms["mainLoginForm"]["lName"].value;
