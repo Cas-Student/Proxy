f0b1d186dabe3a013830baef082ddd3e8414fbd1
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-25 17:21:18 +0000

    l

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import http from 'node:http'
 import { createBareServer } from '@tomphttp/bare-server-node'
 import path from 'node:path'
 import cors from 'cors'
-import config from './config.js']
+import config from './config.js'
 import versions from './info.js';
 console.log("Done");
 
