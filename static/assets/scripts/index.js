2797d854b656e504f4fcee8afc7dcf190e1aee2f
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-08-21 10:37:49 +0000

    inports

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -1,5 +1,5 @@
-import app from '../../../index.js'
-alert("index.js working");
+import express from 'express'
+const app = express()
 
 window.addEventListener('load', () => {
   navigator.serviceWorker.register('../sw.js?v=4', {
