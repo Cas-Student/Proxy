200df5287db0dbab67388984b12c8bb01d10ef5b
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-09-24 13:09:51 -0400

    Tried to un-encode urls
    Signed-off-by: CMacino <123585411+Cas-Student@users.noreply.github.com>

--- index.js
+++ index.js
@@ -60,12 +60,11 @@ for (let user in Accounts) {
 }
 
 console.log("Loading routes")
+app.use(express.json())
+//app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'static')))
 function route() {
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
-  app.use(cors())
-  app.use(express.static(path.join(__dirname, 'static')))
-
   if (config.routes !== false) {
     const routes = [
       { path: '/ap', file: 'apps.html' },
