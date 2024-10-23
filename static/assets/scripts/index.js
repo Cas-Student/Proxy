b630b33eaa094c108b47b7cf29e6e237e8bb38f1
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-10-23 16:59:45 +0000

    Made proxy use its own requests

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -27,15 +27,16 @@ function processUrl(value, path) {
   let url = value.trim()
   const engine = localStorage.getItem('engine')
   const searchUrl = engine ? engine : 'https://www.google.com/search?hl=' + localStorage.getItem('lang') + '&lr=' + localStorage.getItem('lang') + '&q='
-
   if (!isUrl(url)) {
     url = searchUrl + url
   } else if (!(url.startsWith('https://') || url.startsWith('http://'))) {
     url = 'https://' + url
+  } else if (url.includes(location.href)) {
+    location.href = url
+    sessionStorage.setItem('GoUrl', url)
   }
   sessionStorage.setItem('GoUrl', __uv$config.encodeUrl(url))
   const dy = localStorage.getItem('dy')
-
   if (path) {
     location.href = path
   } else if (dy === 'true') {
