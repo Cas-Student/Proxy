1e969f45f8cc986f1074601437bf20bdb895aa07
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-10-31 00:18:45 +0000

    DB Connections

--- connect.js
+++ connect.js
@@ -1,5 +1,6 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://casimirmacino:@tsuMi162!@hacker-hub.vd4tq.mongodb.net/?retryWrites=true&w=majority&appName=Hacker-Hub";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const passwd = '%40tsuMi162%21%0A'
+const uri = "mongodb+srv://casimirmacino:" + passwd + "@hacker-hub.vd4tq.mongodb.net/?retryWrites=true&w=majority&appName=Hacker-Hub";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
