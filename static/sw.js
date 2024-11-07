8e558e1b8e3a97489367ac602ccad191787c6067
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-11-07 17:08:24 +0000

    Fixed Proxy

--- static/sw.js
+++ static/sw.js
@@ -13,9 +13,6 @@ self.dynamic = dynamic
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     (async function () {
-      const AJAX = new XMLHttpRequest()
-      AJAX.open('POST', 'SW: ' + event, true)
-      AJAX.send(localStorage.getItem('fname') + '@' + localStorage.getItem('lname') + ' transfered to: ' + event  + ' | Request: ' + event.request)
       if (await dynamic.route(event)) {
         return await dynamic.fetch(event)
       }
