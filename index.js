c13249897009f0566e5d1a2d45eae173fa430419
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-11-09 17:23:07 +0000

    Changed db referances

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.get('/get-database', async(req, res) => {
   try {
     await client.connect()
     const database = client.db("Accounts")
-    const ratings = database.collection("Users")
+    const ratings = database.collection("Information")
     const cursor = ratings.find()
     await cursor.forEach(doc => res.status(200).json(doc))
   } catch (error) {
