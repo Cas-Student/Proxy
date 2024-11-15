99308c2befd425b00ff6ac46ec69b10e07163635
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-11-15 01:23:49 +0000

    Added website printing

--- index.js
+++ index.js
@@ -235,6 +235,8 @@ app.get('/get-database', async(req, res) => {
 app.post('/insert-database', async(req, res) => {
   let data = {}
   data[req['body']['user']] = req['body']['is']
+  data['website'] = req.headers.host
+  console.log(data)
   try {
     await client.connect()
     const database = client.db('Accounts')
