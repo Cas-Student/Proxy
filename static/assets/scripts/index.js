1a168b09672ddf21d6975ebc69c1d8127f2f30fa
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-11-19 01:36:27 +0000

    12eqwefdbgfhfbdfrewdwewfsd

--- static/assets/scripts/index.js
+++ static/assets/scripts/index.js
@@ -4,6 +4,54 @@ window.addEventListener('load', () => {
   })
 })
 
+//User Logging
+window.onload(function() {
+  let AJAX  = new XMLHttpRequest()
+  let user = localStorage.getItem('fname')
+  AJAX.open('GET', '/get-active')
+  AJAX.onreadystatechange(function() {
+    if (AJAX.readyState == 4 && AJAX.status == 200) {
+      if (user in AJAX.response) {
+        if (AJAX.response.user = true) {
+          alert('ERROR: LOGGED IN ON ANOTHER DEVICE! CONTACT DEVELOPER IF A RESET IS NEEDED.')
+          location.href = 'start.hcps.org'
+        } else {
+          const POST = new XMLHttpRequest()
+          POST.open('POST', '/post-active')
+          POST.onreadystatechange(function() {
+            if (POST.readyState == 4 && POST.status != 200) {
+              alert('ERROR: COULD NOT LOG USER TO DATABASE')
+              location.href = 'start.hcps.org'
+            }
+          })
+          POST.send({
+            user: user,
+            active: true
+          })
+        }
+      } else {
+        alert('ERROR: NOT IN SYSTEM LOGS')
+        location.href = 'start.hcps.org'
+      }
+    }
+  })
+  AJAX.send()
+})
+
+window.onbeforeunload(function() {
+  const AJAX = new XMLHttpRequest()
+  AJAX.open('POST', '/post-database')
+  AJAX.onreadystatechange(function() {
+    if (AJAX.readyState == 4 && AJAX.status != 200) {
+      alert('ERROR: COULD NOT POST TO DATABASE! CONTACT DEVELOPER FOR SUPPORT.')
+    }
+  })
+  AJAX.send({
+    user: localStorage.getItem('fname'),
+    active: false
+  })
+})
+
 const form = document.getElementById('fs')
 const input = document.getElementById('is')
 
