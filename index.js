b4f7837178f4127be8885da1f7d0da30b8c1aef7
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-12-10 02:51:53 +0000

    XD

--- index.js
+++ index.js
@@ -303,7 +303,8 @@ dev.post('/validate-user', async(req, res) => {
           console.log(req.body.user + ' attempted to log in')
           res.status(200).json(msg)
         } else {
-          console.log(data)
+          console.log('Body: ' + req.body)
+          console.log('Response' + data)
           res.status(404).json(data)
         }
       })
