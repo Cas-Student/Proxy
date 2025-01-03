bb2b66f44a3cc326cde6b8d37cc8e10c28146416
CMacino <123585411+Cas-Student@users.noreply.github.com>
2025-01-03 16:46:29 +0000

    removed most sockets

--- index.js
+++ index.js
@@ -112,8 +112,4 @@ const io = new Server(server, {
   cors: { origin: '*' }
 })
 
-msgSocket(io, (socket) => {
-  socket.on('Transfer', (data) => {
-    console.log(`${socket.id} as ${data.user} moved to: ${data.page}`)
-  })
-}, { debug: debug })
+msgSocket(io)
