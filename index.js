2a4ea34d2a5cfbf6cf166d0da3eff95f7b9f03e8
CMacino <123585411+Cas-Student@users.noreply.github.com>
2025-01-12 14:32:18 +0000

    Removed code

--- index.js
+++ index.js
@@ -50,17 +50,6 @@ try {
   } catch (e) {
     console.log(e)
   }
-  function compareStrings(a, b) {
-    // Assuming you want case-insensitive comparison
-    a = a.toLowerCase();
-    b = b.toLowerCase();
-  
-    return (a < b) ? -1 : (a > b) ? 1 : 0;
-  }
-  
-  Accounts.sort(function(a, b) {
-    return compareStrings(a.city, b.city);
-  })
 }
 
 console.log('--------------------')
